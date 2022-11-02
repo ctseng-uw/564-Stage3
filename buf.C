9f3197a42d648ff14622899241c72bf9aa769cc5
Chang-Yen Tseng <10328757+Summon528@users.noreply.github.com>
2022-11-01 20:17:22 -0500

    test

--- buf.C
+++ buf.C
@@ -90,7 +90,7 @@ const Status BufMgr::allocBuf(int& frame)
         }
         if (bufDesc->pinCnt) // The frame is pin, continue and check the next one
         {
-            continue;   
+            continue;      
         }
         // When the control flow flows here, this frame is not pin, not recently used, and not empty, evict this page
         ASSERT(hashTable->remove(FILECASTHACK(bufDesc->file), bufDesc->pageNo) == OK);
@@ -102,8 +102,7 @@ const Status BufMgr::allocBuf(int& frame)
     } while (steps != numBufs * 2); // loop until we circle the clock twice
 
     // If we circle the clock twice, all the frames are PIN and we have to return BUFFEREXCEEDED
-    if (steps == numBufs * 2)
-    {
+    if (steps == numBufs * 2){
         return BUFFEREXCEEDED;
     }
 
